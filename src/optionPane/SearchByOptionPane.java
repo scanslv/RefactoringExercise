ac8f72d72dafbad350e58c3280e7e4d0c3fd942f
Igors Ivanovs <scanslv@gmail.com>
2018-02-23 12:16:16 +0000

    Add/change validation

--- src/optionPane/SearchByOptionPane.java
+++ src/optionPane/SearchByOptionPane.java
@@ -19,36 +19,40 @@ public class SearchByOptionPane {
 
     private void searchBySurname() {
         String sName = JOptionPane.showInputDialog(Constants.SEARCH_FOR_SURNAME);
-        boolean found = false;
+        if (sName != null) {
+            boolean found = false;
 
-        for (BankAccount bankAccount : parent.table) {
-            if (sName.equalsIgnoreCase((bankAccount.getSurname().trim()))) {
-                found = true;
-                parent.currentItem = parent.table.indexOf(bankAccount);
-                parent.displayDetails();
+            for (BankAccount bankAccount : parent.table) {
+                if (sName.equalsIgnoreCase((bankAccount.getSurname().trim()))) {
+                    found = true;
+                    parent.currentItem = parent.table.indexOf(bankAccount);
+                    parent.displayDetails();
+                }
             }
+            if (found)
+                JOptionPane.showMessageDialog(null, Constants.SURNAME + sName + Constants.FOUND);
+            else
+                JOptionPane.showMessageDialog(null, Constants.SURNAME + sName + Constants.NOT_FOUND);
         }
-        if (found)
-            JOptionPane.showMessageDialog(null, Constants.SURNAME + sName + Constants.FOUND);
-        else
-            JOptionPane.showMessageDialog(null, Constants.SURNAME + sName + Constants.NOT_FOUND);
     }
 
     private void searchByAccNr() {
         String accNum = JOptionPane.showInputDialog(Constants.SEARCH_FOR_ACC_NR);
-        boolean found = false;
+        if (accNum != null) {
+            boolean found = false;
 
-        for (BankAccount bankAccount : parent.table) {
-            if (accNum.equals(bankAccount.getAccountNumber().trim())) {
-                found = true;
-                parent.currentItem = parent.table.indexOf(bankAccount);
-                parent.displayDetails();
+            for (BankAccount bankAccount : parent.table) {
+                if (accNum.equals(bankAccount.getAccountNumber().trim())) {
+                    found = true;
+                    parent.currentItem = parent.table.indexOf(bankAccount);
+                    parent.displayDetails();
 
+                }
             }
+            if (found)
+                JOptionPane.showMessageDialog(null, Constants.ACCOUNT_NR + accNum + Constants.FOUND);
+            else
+                JOptionPane.showMessageDialog(null, Constants.ACCOUNT_NR + accNum + Constants.NOT_FOUND);
         }
-        if (found)
-            JOptionPane.showMessageDialog(null, Constants.ACCOUNT_NR + accNum + Constants.FOUND);
-        else
-            JOptionPane.showMessageDialog(null, Constants.ACCOUNT_NR + accNum + Constants.NOT_FOUND);
     }
 }
